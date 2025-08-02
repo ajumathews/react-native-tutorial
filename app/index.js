fd341d089b60201449b2e6c4c797b131776432c5
ajumathews <aju.mathewsamuel@gmail.com>
2025-08-02 13:18:18 -0700

    Add multiple pages

--- app/index.js
+++ app/index.js
@@ -1,17 +1,23 @@
 import { Image, StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
+
 import Logo from "../assets/img/logo_light.png";
 
 const Home = () => {
   return (
     <View style={styles.container}>
       <Image source={Logo} style={styles.img} />
+
       <Text style={styles.title}>The Number 1</Text>
 
       <Text style={{ marginTop: 10, marginBottom: 30 }}>Reading List App</Text>
 
-      <View style={styles.card}>
-        <Text>Hello, this is a Card</Text>
-      </View>
+      <Link href="/about" style={styles.link}>
+        About Page
+      </Link>
+      <Link href="/contact" style={styles.link}>
+        Contact Page
+      </Link>
     </View>
   );
 };
@@ -26,16 +32,14 @@ const styles = StyleSheet.create({
     justifyContent: "center"
   },
   img: {
-    margin: 20
+    marginVertical: 20
   },
   title: {
     fontWeight: "bold",
     fontSize: 18
   },
-  card: {
-    backgroundColor: "#fff",
-    padding: 20,
-    borderRadius: 5,
-    boxShadow: "4px 4px rgba(0,0,0,0.1)"
+  link: {
+    marginVertical: 10,
+    borderBottomWidth: 1
   }
 });

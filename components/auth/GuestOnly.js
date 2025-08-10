7dba7f56449c86b3a8bb76362177c29e46e41183
ajumathews <aju.mathewsamuel@gmail.com>
2025-08-10 16:18:10 -0700

    Redirect users after login and logout

--- components/auth/GuestOnly.js
+++ components/auth/GuestOnly.js
@@ -3,21 +3,21 @@ import { useRouter } from "expo-router";
 import { useEffect } from "react";
 import { Text } from "react-native";
 
-const UserOnly = ({ children }) => {
+const GuestOnly = ({ children }) => {
   const { user, authChecked } = useUser();
   const router = useRouter();
 
   useEffect(() => {
-    if (authChecked && user === null) {
-      router.replace("/login");
+    if (authChecked && user !== null) {
+      router.replace("/profile");
     }
   }, [user, authChecked]);
 
-  if (!authChecked || !user) {
+  if (!authChecked || user) {
     return <Text>Loading</Text>;
   }
 
   return children;
 };
 
-export default UserOnly;
+export default GuestOnly;

73178848e9acb37380f2974cd56996e0a3b193a1
ajumathews <aju.mathewsamuel@gmail.com>
2025-08-10 13:28:33 -0700

    Fixed initial screen

--- app/_layout.js
+++ app/_layout.js
@@ -16,12 +16,10 @@ export default function RootLayout() {
           headerTintColor: theme.title
         }}
       >
-        {/* Groups */}
+        <Stack.Screen name="index" options={{ title: "Home" }} />
+
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="(dashboard)" options={{ headerShown: false }} />
-
-        {/* Individual Screens */}
-        <Stack.Screen name="index" options={{ title: "Home" }} />
       </Stack>
     </>
   );

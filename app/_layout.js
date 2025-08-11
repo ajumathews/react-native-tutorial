475c8d1010ff3d1ee6fce24618d6fe5aae41cfa3
ajumathews <aju.mathewsamuel@gmail.com>
2025-08-10 17:03:44 -0700

    List Books

--- app/_layout.js
+++ app/_layout.js
@@ -3,6 +3,7 @@ import { Colors } from "../constants/Colors";
 import { useColorScheme } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { UserProvider } from "../contexts/UserContext";
+import { BooksProvider } from "../contexts/BooksContext";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -10,18 +11,20 @@ export default function RootLayout() {
 
   return (
     <UserProvider>
-      <StatusBar value="auto" />
-      <Stack
-        screenOptions={{
-          headerStyle: { backgroundColor: theme.navBackground },
-          headerTintColor: theme.title
-        }}
-      >
-        <Stack.Screen name="index" options={{ title: "Home" }} />
+      <BooksProvider>
+        <StatusBar value="auto" />
+        <Stack
+          screenOptions={{
+            headerStyle: { backgroundColor: theme.navBackground },
+            headerTintColor: theme.title
+          }}
+        >
+          <Stack.Screen name="index" options={{ title: "Home" }} />
 
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-        <Stack.Screen name="(dashboard)" options={{ headerShown: false }} />
-      </Stack>
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          <Stack.Screen name="(dashboard)" options={{ headerShown: false }} />
+        </Stack>
+      </BooksProvider>
     </UserProvider>
   );
 }
---
+++ contexts/BooksContext.js
@@ -0,0 +1,57 @@
+import { createContext, useEffect, useState } from "react";
+import { databases } from "../lib/appwrite";
+import { ID, Permission, Query, Role } from "react-native-appwrite";
+import { useUser } from "../hooks/useUser";
+
+const DATABASE_ID = "68992cfb00130953b8cd";
+const COLLECTION_ID = "68992d03000f70d9d59e";
+
+export const BooksContext = createContext();
+
+export function BooksProvider({ children }) {
+  const [books, setBooks] = useState([]);
+  const { user } = useUser();
+
+  async function fetchBooks() {
+    try {
+      const response = await databases.listDocuments(DATABASE_ID, COLLECTION_ID, [Query.equal("userId", user.$id)]);
+
+      setBooks(response.documents);
+      console.log(response.documents);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  async function fetchBookById(id) {
+    try {
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  async function createBook(data) {
+    try {
+      await databases.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), { ...data, userId: user.$id }, [Permission.read(Role.user(user.$id)), Permission.update(Role.user(user.$id)), Permission.delete(Role.user(user.$id))]);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  async function deleteBook(id) {
+    try {
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  useEffect(() => {
+    if (user) {
+      fetchBooks();
+    } else {
+      setBooks([]);
+    }
+  }, [user]);
+
+  return <BooksContext.Provider value={{ books, fetchBooks, fetchBookById, createBook, deleteBook }}>{children}</BooksContext.Provider>;
+}
